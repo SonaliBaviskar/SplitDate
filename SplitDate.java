17a1a8dbf763420c352f136cb0bdfdaf51480a43
Sonali Baviskar <85393208+SonaliBaviskar@users.noreply.github.com>
2021-07-11 18:19:36 +0530

    Add files via upload

---
+++ SplitDate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class SplitDate
+{
+public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        String x = sc.nextLine();
+        String[] N =x.split("-");
+        System.out.println("Day: " +N[0]);
+        System.out.println("Month: " +N[1]);
+        System.out.println("Year: " +N[2]);
+        
+    }
+}
